58fa9bffc933112f247d0db47d2632a15596b3c5
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-06-06 10:48:21 -0400

    test commit?

--- script.js
+++ script.js
@@ -26,8 +26,26 @@
 //else unmatched fucntion
 //*************************************** */
 //match function needs to update state of the div once two correct cards are matched, turn off event listener
-//cardpair 1 & 2 = blocked
+//cardpair 1 & 2 = blocked(event listener off)
 //cardpair array is emptied again
 //query previous score and score++
 //************************************************ */
-//
+//unmatch function// want cards to flip completely over if they are an unmatched pair.
+// setTimeouit 
+//card1 & card2.style.transform ), 750)
+//repeat above **cardpair 1 and 2 = blocked
+//pair of cards array empty again
+//************************************************** */
+//game timer function
+//need to reference
+
+//************************* */
+//shuffle card function
+//query select IMG
+//declare array of pictures
+//need to invoke towards beginning of code
+
+/// for loop to shuffle cards
+
+
+
