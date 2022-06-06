344e87f10b4c1275cffcdb15481355251a97c9a8
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-06-06 10:47:40 -0400

    some psuedo complete

---
+++ script.js
@@ -0,0 +1,33 @@
+///going to need 4 divs for each card in HTML
+//need to name Div for query selector
+///////////////////////////////////////////////////////////
+
+//use queryselector to get the div I want
+//declare variable for empty array
+//use boolean to declare the first click as false
+
+//*********************************************************** */
+//card [] event listener to click
+// upon first click initiate timer
+
+//*********************************************** */
+//if cards[] = unclicked
+//cards [] transform
+//card[] = clicked
+//push card to cardpair
+//checked function
+//else if cards [] = clicked
+// transform nothing
+// pair of cards array empty
+
+//***************************** */
+// function to check cards
+//if card[0] = card[1] = match fucntion
+//else unmatched fucntion
+//*************************************** */
+//match function needs to update state of the div once two correct cards are matched, turn off event listener
+//cardpair 1 & 2 = blocked
+//cardpair array is emptied again
+//query previous score and score++
+//************************************************ */
+//
